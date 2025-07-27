bd3c82307c13e515a1fa97a5964291fba340a3d0
itsoliviasparks <97206055+itsoliviasparks@users.noreply.github.com>
2025-07-27 13:36:24 -0600

    updaint hosting to Firebase, upating URLs

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <footer>
-            <p>Invented by <a href='https://christianyuen.com' target='_blank' aria-label='link text - new window'>Christian Yuen</a>, <a href='https://mattspencercodes.com/' target='_blank' aria-label='link text - new window'>Matt Spencer</a>,</p>
-            <p><a href='https://itsoliviasparks.com' target='_blank' aria-label='link text - new window'>Olivia Sparks</a>, & <a href='https://samjaberi.dev/' target='_blank' aria-label='link text - new window'>Sam Jaberi</a>.</p>
+            <p>Invented by <a href='https://github.com/christianyuen' target='_blank' aria-label='link text - new window'>Christian Yuen</a>, <a href='https://github.com/Matt01270' target='_blank' aria-label='link text - new window'>Matt Spencer</a>,</p>
+            <p><a href='https://github.com/itsoliviasparks' target='_blank' aria-label='link text - new window'>Olivia Sparks</a>, & <a href='https://github.com/samnjab' target='_blank' aria-label='link text - new window'>Sam Jaberi</a>.</p>
             <p><a href='https://github.com/itsoliviasparks/back-to-the-acronym' target='_blank' aria-label='link text - new window'>Project Repo</a></p>
             <p>"<a href='https://junocollege.com' target='_blank' aria-label='link text - new window'>Juno College</a>, My Density Has Brought Me To You."</p>
         </footer>
